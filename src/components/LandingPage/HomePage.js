2182a71dc62864ee3d56653334a17ecfc2d54af0
lvthezh <lvthe.zh@gmail.com>
2022-09-09 18:29:29 +0700

    ....

--- src/components/LandingPage/HomePage.js
+++ src/components/LandingPage/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import headerRight from '../../assets/header-right.png';
 import { makeStyles, Button, Typography } from '@material-ui/core';
-import image from '../../assets/background_bk.jpg';
+import image from '../../assets/background_home.png';
 
 const useStyles = makeStyles((theme) => ({
   background: {
