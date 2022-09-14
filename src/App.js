551364805abee8f3e656d3becbad6945facc2521
dvGiau <doanvangiau95@gmail.com>
2022-09-14 15:33:16 +0700

    [dvGiau] App.js add styleAppjs

--- src/App.js
+++ src/App.js
@@ -7,9 +7,19 @@ import Navbar from './components/common/Navbar';
 import { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import CirculatingSupply from './components/CirculatingSupply';
-import TotalSupply from './components/TotalSupply';
-
+import TotalSupply from './components/TotalSupply';  
+import { makeStyles, Button, Typography } from '@material-ui/core';
+const styleAppjs = makeStyles((theme) => ({
+  styleAppjs_Body: {
+    overflow: 'hidden',
+    padding: '0px', 
+    width:"100%",
+    height:'auto'
+  }
+  // className='d-flex no-wrap col-s-12 col-12 col-lg-12 col-md-12 pa-0'
+}));
 function App() {
+  const cssClassAppjs = styleAppjs();
   return (
     <ThemeProvider theme={theme}>
       <Router>
@@ -18,7 +28,7 @@ function App() {
             {/* <Route exact path="/">  className="appbar-section" */}
             <Route basename="/SoulWord">
               <section>
-                <div className='d-flex no-wrap col-s-12 col-12 col-lg-12 col-md-12 pa-0'>
+                <div className={cssClassAppjs.styleAppjs_Body}>
                 <Navbar />
                 <LandingPage />
                 </div>
